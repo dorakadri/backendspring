a61e3e4f49e565cc06f858162a472d3dd84a06b9
dorakadri <dora.kadri@esprit.tn>
2022-12-14 02:32:18 +0100

    testeepp

--- src/main/java/tn/esprit/khaddempro/configuration/CorsConfiguration.java
+++ src/main/java/tn/esprit/khaddempro/configuration/CorsConfiguration.java
@@ -18,7 +18,7 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("**/**")
                         .allowedMethods(GET, POST, PUT, DELETE)
                         .allowedHeaders("*")
                         .allowedOriginPatterns("*")
