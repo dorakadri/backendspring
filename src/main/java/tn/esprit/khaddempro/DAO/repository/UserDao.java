1850a86b1458cdf1fac4fe6a35474f1003aa08d9
dorakadri <dora.kadri@esprit.tn>
2022-12-14 03:01:11 +0100

    testeepghphtt

--- src/main/java/tn/esprit/khaddempro/DAO/repository/UserDao.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.khaddempro.DAO.repository;
-
-import tn.esprit.khaddempro.DAO.entities.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends CrudRepository<User, String> {
-}
